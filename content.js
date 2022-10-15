ac7e725b383114e118cbe7b4376859d9c5825d57
addavriance <61160742+addavriance@users.noreply.github.com>
2022-10-15 15:42:51 +0300

    микро изменение функций, ссылка берется из popup'a

--- content.js
+++ content.js
@@ -6,37 +6,54 @@
 // }
 
 
-var myLink = 'https://i.yapx.ru/OqAKA.gif'
+chrome.storage.sync.get(['key1'], function(result) {
+    localStorage.link1=result.key1
+});
+
+var myLink=localStorage.link1
 var otherLink = 'https://wallpaperaccess.com/full/2641074.gif'
+
 var myHTML = '<div class="OwnerPageCover__in" style="background-image: url(&quot;'+myLink+'&quot;);"><div class="OwnerPageCover__button"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Чтобы изменить обложку отключите расширение</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>'
 var otherHTML='<div class="OwnerPageCover__in" style="background-image: url(&quot;'+otherLink+'&quot;);"></div>'
-function checkonload(click=false){
-    var timing
-    if(click){timing=970} else {timing=360}
-        setTimeout(window.onload = function(){
-            try{
-                if(document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML.length>300){
-                    document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=myHTML
-                } else {
-                    document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=otherHTML
-                }
-            } catch (e) {
-                console.log("Cover finding failure.")
-            }
-        }, timing)
-    }
-checkonload()
+
 var targetNode = document.getElementById('utils');
 var config = { attributes: true, childList: true };
+
 var callback = function(mutationsList) {
     for(var mutation of mutationsList) {
         if (mutation.type == 'childList') {
-            checkonload(true)
+            console.log("childlist change")
+            checkonload(false)
         }
         else if (mutation.type == 'attributes') {
-            checkonload(true)
+            console.log("attribute change")
+            checkonload(false)
         }
     }
 };
 var observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+
+function checkonload(fail){
+    if (!fail)
+        console.log("сover finding started.")
+
+        setTimeout(window.onload = function(){
+            try{
+                if(document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML.length>300){
+                    document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=myHTML
+                    console.log("myHTML loaded.")
+                    
+                } else if (document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML.length!=38){
+                    document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=otherHTML
+                    console.log("otherHTML loaded.")
+                } else {
+                    console.log("cover load failure, retrying...")
+                    checkonload(true)
+                }
+            } catch (e) {
+                console.log("сover finding failure.")
+            }
+        }, 1000)
+}
+
+try{observer.observe(targetNode, config);} catch(e){}
