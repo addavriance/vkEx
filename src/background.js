b5cd3c06687ba7b8f99be4810ad8fad90a67362a
addavriance <netiazhenko@mail.ru>
2022-10-17 22:18:53 +0300

    листенер клика по иконке
    кидает сообщение из background.js и другой листенер ловит его в content.js

---
+++ src/background.js
@@ -0,0 +1,5 @@
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});  
+});
+});
