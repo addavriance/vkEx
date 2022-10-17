4cd35cb56c96f9a79dbe3c9d9828c7d94944ad28
addavriance <netiazhenko@mail.ru>
2022-10-17 22:19:25 +0300

    + вкл/выкл расширение по клику по иконке

--- src/content.js
+++ src/content.js
@@ -1,6 +1,10 @@
 import { BrowserQRCodeReader } from '@zxing/browser'
 var HTMLparts = ['<div class="OwnerPageCover__in" style="background-image: url(&quot;','&quot;);"><div class="OwnerPageCover__button"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Изменить обложку</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>' ,'&quot;);"><div class="OwnerPageCover__button"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Чтобы изменить обложку отключите расширение</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>', '&quot;);"></div>']
 var loadingHTML = HTMLparts[0]+'https://venturebeat.com/wp-content/uploads/2014/10/loading_desktop_by_brianmccumber-d41z4h6.gif?w=1200&strip=all'+HTMLparts[3]
+var start_HTML='';
+if(localStorage.working==undefined){
+    localStorage.working=true
+}
 function getURL(html='', my=false){
         var url=html.replace(HTMLparts[0], '').replace(HTMLparts[1],'').replace('&quot;);"></div>', '').replace('&quot;);"><div class="OwnerPageCover__button OwnerPageCover__button--shown"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse vkuiTappable--hover-background"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Изменить обложку</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>', '')
         document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML = loadingHTML
@@ -20,7 +24,7 @@ function scanQR(url, my){
     var i = setInterval(function(){
         counter+=1; 
         if (counter==5){
-            document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=HTMLparts[0]+''+HTMLparts[3];
+            document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=start_HTML
             clearInterval(i)
         }
         if (link!='')
@@ -61,13 +65,15 @@ var callback = function(mutationsList) {
 var observer = new MutationObserver(callback);
 
 function checkonload(fail){
+    if(localStorage.working=='true'){
     if (!fail)
         //console.log("сover finding started.")
 
         setTimeout(window.onload = function(){
             try{    
                 if (document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML.length!=38){
-                    getURL(document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML, document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML.length>300)
+                    start_HTML=document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML;
+                    getURL(start_HTML, start_HTML.length>300)
                 } else {
                     console.log("cover load failure, retrying...")
                     checkonload(true)
@@ -76,8 +82,22 @@ function checkonload(fail){
                 console.log("сover finding failure.")
             }
         }, 1000)
+    }
 }
 
 window.onload=checkonload(false)
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    localStorage.working=localStorage.working=='true' ? false : true
+
+    if(localStorage.working=='true'){
+        alert("VK Covers: Расширение включено, перезагрузите страницу.")
+        // chrome.browserAction.setTitle("VK Covers: анимированные обложки включены")
+    } else {
+        alert("VK Covers: Расширение отключено, перезагрузите страницу.")
+        // chrome.browserAction.setTitle("VK Covers: анимированные обложки в отключены")
+    }
+    return true
+});
 
 try{observer.observe(targetNode, config);} catch(e){console.log("observer error")}
