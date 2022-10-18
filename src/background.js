d123f8f89287c42ab4360ef20da9ab846b1c0fc2
addavriance <netiazhenko@mail.ru>
2022-10-18 22:02:10 +0300

    Изменение иконки и тайтла по клику

--- src/background.js
+++ src/background.js
@@ -1,5 +1,21 @@
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});  
-});
-});
+var working;
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  working = message.working
+  if(working=='true'){
+    chrome.action.setTitle({title: "VK Covers: выключить анимированные обложки"})
+    chrome.action.setIcon({path: {"38":"/icons/icon_on_38.png"}})
+  } else {
+    chrome.action.setTitle({title: "VK Covers: включить анимированные обложки"})
+    chrome.action.setIcon({path: {"38":"/icons/icon_off_38.png"}})
+  }
+  return true
+})
+try{
+    chrome.action.onClicked.addListener(function (tab) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        // if (tabs[0].url.match('https:\/\/.*.vk.com\/.*')) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});  
+        // }
+      });
+    });
+} catch(e){console.log(e)}
