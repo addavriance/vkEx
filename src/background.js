eaf6b4636924f7307cd3717cc02778b59d8c18c9
addavriance <netiazhenko@mail.ru>
2022-10-22 22:21:34 +0300

    temp. disabled extension on/off func.

--- src/background.js
+++ src/background.js
@@ -1,21 +1,21 @@
-var working;
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  working = message.working
-  if(working=='true'){
-    chrome.action.setTitle({title: "VK Covers: выключить анимированные обложки"})
-    chrome.action.setIcon({path: {"38":"/icons/icon_on_38.png"}})
-  } else {
-    chrome.action.setTitle({title: "VK Covers: включить анимированные обложки"})
-    chrome.action.setIcon({path: {"38":"/icons/icon_off_38.png"}})
-  }
-  return true
-})
-try{
-    chrome.action.onClicked.addListener(function (tab) {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        // if (tabs[0].url.match('https:\/\/.*.vk.com\/.*')) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});  
-        // }
-      });
-    });
-} catch(e){console.log(e)}
+// var working;
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   working = message.working
+//   if(working=='true'){
+//     chrome.action.setTitle({title: "VK Covers: выключить анимированные обложки"})
+//     chrome.action.setIcon({path: {"38":"/icons/icon_on_38.png"}})
+//   } else {
+//     chrome.action.setTitle({title: "VK Covers: включить анимированные обложки"})
+//     chrome.action.setIcon({path: {"38":"/icons/icon_off_38.png"}})
+//   }
+//   return true
+// })
+// try{
+//     chrome.action.onClicked.addListener(function (tab) {
+//       chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//         // if (tabs[0].url.match('https:\/\/.*.vk.com\/.*')) {
+//         chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});  
+//         // }
+//       });
+//     });
+// } catch(e){console.log(e)}
