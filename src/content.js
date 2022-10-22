77d92bde4ce32cd31c0b2fdb7c89a84199bdfc38
addavriance <netiazhenko@mail.ru>
2022-10-22 22:22:04 +0300

    add qr installer

--- src/content.js
+++ src/content.js
@@ -1,9 +1,21 @@
-import { BrowserQRCodeReader } from '@zxing/browser'
+/*/outerHTML used only for getting and replacing VK user cover preview and nothing else.(extension adds animated covers for every user
+who placed qr-code on his page cover)/*/
+
+import { BrowserQRCodeReader, BrowserQRCodeSvgWriter } from '@zxing/browser'
+import * as QRCode from 'qrcode'
+
 var HTMLparts = ['<div class="OwnerPageCover__in" style="background-image: url(&quot;','&quot;);"><div class="OwnerPageCover__button"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Изменить обложку</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>' ,'&quot;);"><div class="OwnerPageCover__button"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Чтобы изменить обложку отключите расширение</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>', '&quot;);"></div>']
 var loadingHTML = HTMLparts[0]+'https://venturebeat.com/wp-content/uploads/2014/10/loading_desktop_by_brianmccumber-d41z4h6.gif?w=1200&strip=all'+HTMLparts[3]
 var start_HTML='';
-localStorage.working = localStorage.working == undefined ? localStorage.working = true : localStorage.working;
-chrome.runtime.sendMessage(null, {working: localStorage.working}, function(response) {});
+var HTML=''
+
+var targetNode = document.getElementById('wrap3');
+var config = { attributes: true, childList: true };
+
+// localStorage.working = localStorage.working == undefined ? localStorage.working = true : localStorage.working;
+
+// chrome.runtime.sendMessage(null, {working: localStorage.working}, function(response) {});
+
 function getURL(html='', my=false){
         var url=html.replace(HTMLparts[0], '').replace(HTMLparts[1],'').replace('&quot;);"></div>', '').replace('&quot;);"><div class="OwnerPageCover__button OwnerPageCover__button--shown"><button type="button" class="vkuiButton vkuiButton--sz-m vkuiButton--lvl-secondary vkuiButton--clr-overlay vkuiButton--aln-center vkuiButton--sizeY-compact vkuiButton--with-icon vkuiTappable vkuiTappable--sizeX-regular vkuiTappable--hasHover vkuiTappable--hasActive vkuiTappable--mouse vkuiTappable--hover-background"><span class="vkuiButton__in"><span role="presentation" class="vkuiButton__before"><div role="presentation" class=" vkuiIcon vkuiIcon--24 vkuiIcon--w-24 vkuiIcon--h-24 vkuiIcon--pen_outline_24 " style="width: 24px; height: 24px;"><svg viewBox="0 0 24 24" width="24" height="24" style="display: block;"><use xlink:href="#pen_outline_24" style="fill: currentcolor;"></use></svg></div></span><span class="vkuiButton__content vkuiSubhead vkuiSubhead--sizeY-compact vkuiSubhead--w-2">Изменить обложку</span></span><span aria-hidden="true" class="vkuiTappable__hoverShadow"></span><span aria-hidden="true" class="vkuiFocusVisible vkuiFocusVisible--outside"></span></button></div></div>', '')
         document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML = loadingHTML
@@ -13,9 +25,11 @@ function getURL(html='', my=false){
 function scanQR(url, my){
     var link = ''
     var counter = 0
+
     var imgObj = new Image();
     imgObj.src = url + '?' + new Date().getTime();
     imgObj.setAttribute('crossOrigin', '');
+
     const codeReader = new BrowserQRCodeReader()
     codeReader.decodeFromImageElement(imgObj).then(result => 
         link = result.getText()
@@ -34,7 +48,6 @@ function scanQR(url, my){
     }, 1000)
 };
     
-var HTML=''
 function solveHTML(link, my){
     //console.log("Solving HTML...")
     if(my){
@@ -45,9 +58,6 @@ function solveHTML(link, my){
     document.getElementsByClassName("OwnerPageCover__in")[0].outerHTML=HTML
     //console.log("HTML loaded.")
 }
-
-var targetNode = document.getElementById('wrap3');
-var config = { attributes: true, childList: true };
 
 var callback = function(mutationsList) {
     for(var mutation of mutationsList) {
@@ -78,22 +88,46 @@ function checkonload(fail){
                     checkonload(true)
                 }
             } catch (e) {
-                console.log("сover finding failure.")
+                // console.log("сover finding failure.")
             }
         }, 1000)
     }
 }
 
+function writeQR(url){
+    const qrOption = { 
+        margin: 35,
+        width : 1000,
+      };
+    QRCode.toDataURL(url, qrOption).then(res=>{
+        var data = res;
+        var arr = data.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        var file = new File([u8arr], "cover.png", {type:mime});
+
+        let a = document.createElement("a");
+        a.href = URL.createObjectURL(file);
+        a.download = "cover.png";
+        a.click();
+    })
+}
+
 window.onload=checkonload(false)
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if(localStorage.working=='true'){
-        localStorage.working='false'
-    } else {
-        localStorage.working='true'
-    }
-    chrome.runtime.sendMessage(null, {working: localStorage.working}, function(response) {});
+    // if(localStorage.working=='true'){
+    //     localStorage.working='false'
+    // } else {
+    //     localStorage.working='true'
+    // }
+    // chrome.runtime.sendMessage(null, {working: localStorage.working}, function(response) {});
+    writeQR(message.url)
     return true
 });
+
+var observer = new MutationObserver(callback);
 
 try{observer.observe(targetNode, config);} catch(e){console.log("observer error")}
