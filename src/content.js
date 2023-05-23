2463a78b48e483e7d216d10587649811ce44d2b6
addavriance <netiazhenko@mail.ru>
2023-05-23 18:05:48 +0300

    Refactored cover changing logic

--- src/content.js
+++ src/content.js
@@ -1,87 +1,62 @@
 /*/outerHTML used only for getting and replacing VK user cover preview and nothing else. (extension adds animated covers for every user
 who placed qr-code on his page cover)/*/
 
-import { BrowserQRCodeReader } from '@zxing/browser'
+import { BrowserQRCodeReader } from '@zxing/browser';
 
-var loadingURL = "url('https://venturebeat.com/wp-content/uploads/2014/10/loading_desktop_by_brianmccumber-d41z4h6.gif?w=1200&strip=all')"
+const loadingURL = "url('https://venturebeat.com/wp-content/uploads/2014/10/loading_desktop_by_brianmccumber-d41z4h6.gif?w=1200&strip=all')";
 
-var targetNode = getElementByXpath("/html/body");
-var config = { attributes: true, childList: true };
-var Gcounter = 0;
-var counter = 0
 
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+function scanQRCode(obj, url, currentURL) {
+  const imgObj = new Image();
+  imgObj.src = url + '?' + new Date().getTime();
+  imgObj.setAttribute('crossOrigin', '');
+
+  const codeReader = new BrowserQRCodeReader();
+  codeReader.decodeFromImageElement(imgObj)
+    .then(result => {
+      obj.style.backgroundImage = `url("${result.getText()}")`;
+    })
+    .catch(() => {
+      obj.style.backgroundImage = currentURL;
+      console.log("QR scan failed");
+    });
 }
 
-function getURL(Obj){
-    var cUrl = Obj.style.backgroundImage;
-    var url = Obj.style.backgroundImage.replace('url("', "").replace('")', '');
-
-    Obj.style.backgroundImage = loadingURL;
-
-    scanQR(Obj, url, cUrl);
-    }
-
-function scanQR(Obj, url, cUrl){
-    if(!counter) {
-        counter++;
-        var link = '';
-        var imgObj = new Image();
-
-        imgObj.src = url + '?' + new Date().getTime();
-        imgObj.setAttribute('crossOrigin', '');
-
-        const codeReader = new BrowserQRCodeReader()
-        codeReader.decodeFromImageElement(imgObj).then(result => 
-            link = `url("${result.getText()}")`
-        ).catch(()=>{ console.log("qr-scan failed") });
-
-        var i = setInterval(function(){
-            counter++;
-            counter>=4 ? !!(Obj.style.backgroundImage=cUrl) && !(counter = 0) && clearInterval(i) : void 0;
-            !!link ? !!(Obj.style.backgroundImage=link) && !(counter = 0) && clearInterval(i) : void 0
-        }, 1000);
-
-    }
-};
-
-var callback = function(mutationsList) {
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            checkonload();
-        }
-        else if (mutation.type == 'attributes') {
-            checkonload();
-        }
-    }
-};
-
-var observer = new MutationObserver(callback);
-
-function checkonload(){
-    var coverObj;
-    if(Gcounter<=3){
-        try {  
-            if (document.getElementsByClassName("OwnerPageCover__in").length > 1) {
-                var e;
-                Gcounter = 0;
-                coverObj=!!(e = document.getElementsByClassName("OwnerPageCover__in"))[0].textContent ? e[0] : e[1];
-                getURL(coverObj);
-            } else {
-                Gcounter++;
-                setTimeout(()=>{checkonload()}, 1000);
-            }
-        } catch (e) {
-            console.log("cover finding failure.")
-        }
-    } else {
-        Gcounter = 0;
-    }
+function processProfileWrapper() {
+  console.log("Processing cover...");
+  const coverObj = document.getElementsByClassName("OwnerPageCover__in")[1];
+  if (coverObj) {
+    getURL(coverObj);
+  }
 }
 
-window.onload = checkonload();
+function trackProfileWrapper() {
+  const observer = new MutationObserver(mutationsList => {
+    mutationsList.forEach(mutation => {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach(node => {
+          if (!!node.classList && (node.classList.contains('ProfileWrapper') || (!!node.src && node.src.includes("userapi.com") && !!node.classList.value))) {
+            setTimeout(() => {
+              processProfileWrapper();
+            }, 500);
+        }
+      });
+    }
+  });
+});
 
-var observer = new MutationObserver(callback);
+observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+}
 
-try { observer.observe(targetNode, config) } catch(e) { console.log("observer error") };
+function getURL(obj) {
+  const cUrl = obj.style.backgroundImage;
+  const url = obj.style.backgroundImage.replace('url("', "").replace('")', '');
+
+  obj.style.backgroundImage = loadingURL;
+
+  scanQRCode(obj, url, cUrl);
+}
+trackProfileWrapper();
